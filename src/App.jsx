e8f34752b9300a9b89d074846f8dc969b9f93132
PRiNcE3223 <you@example.com>
2025-05-01 20:35:49 +0530

    here

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div><h1>MY REACT APP</h1>
+    <h2>CI/CD PIPELINE IMPLEMENTED</h2>
+    <h3>WITH GITHUB ACTION</h3>
+    
+    </div>
+   
+  )
+}
+
+export default App
